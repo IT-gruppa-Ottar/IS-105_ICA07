14acdc5dbd763a11144a12e035b401aeb08d823c
Christoffer Lislevand <chrirl16@student.uia.no>
2017-05-11 14:05:16 +0200

    Oppgave 1 - Implementering av kryptering

--- Oppgave 1/client.go
+++ Oppgave 1/client.go
@@ -4,34 +4,37 @@ import (
 	"fmt"
 	"net"
 	"time"
+	"./encryption"
 )
-
-func CheckError(err error) {
-	if err  != nil {
-		fmt.Println("Error: " , err)
-	}
-}
 
 func main() {
 	ServerAddr,err := net.ResolveUDPAddr("udp","127.0.0.1:10001")
-	CheckError(err)
+	checkError(err)
 
 	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
-	CheckError(err)
+	checkError(err)
 
 	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
-	CheckError(err)
+	checkError(err)
 
 	defer Conn.Close()
 	i := 0
 	for {
 		msg := "Møte Fr 5.5 14:45 Flåklypa"
 		i++
-		buf := []byte(msg)
-		_,err := Conn.Write(buf)
+		encryptedMessage := encryption.EncryptMessage(msg)
+
+		//buf := []byte(msg)
+		_,err := Conn.Write(encryptedMessage)
 		if err != nil {
-			fmt.Println(msg, err)
+			fmt.Println(encryptedMessage, err)
 		}
 		time.Sleep(time.Second * 1)
+	}
+}
+
+func checkError(err error) {
+	if err  != nil {
+		fmt.Println("Error: " , err)
 	}
 }
--- Oppgave 1/server.go
+++ Oppgave 1/server.go
@@ -4,15 +4,10 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"./encryption"
 )
 
-/* A Simple function to verify error */
-func CheckError(err error) {
-	if err  != nil {
-		fmt.Println("Error: " , err)
-		os.Exit(0)
-	}
-}
+
 
 func main() {
 	/* Lets prepare a address at any address at port 10001*/
@@ -26,12 +21,21 @@ func main() {
 
 	buf := make([]byte, 1024)
 
-	for {
-		n,addr,err := ServerConn.ReadFromUDP(buf)
-		fmt.Println("Received ",string(buf[0:n]), " from ",addr)
+	n,addr,err := ServerConn.ReadFromUDP(buf)
 
-		if err != nil {
-			fmt.Println("Error: ",err)
-		}
+	decryptedMessage := encryption.DecryptMessage(buf)
+
+	fmt.Println("Received ",string(decryptedMessage[0:n]), " from ",addr)
+
+	if err != nil {
+		fmt.Println("Error: ",err)
+	}
+}
+
+/* A Simple function to verify error */
+func CheckError(err error) {
+	if err  != nil {
+		fmt.Println("Error: " , err)
+		os.Exit(0)
 	}
 }
